8db7ea0042d80c6347d2a2381c8850e7b00b9a4e
maggie197 <megijagabalina@gmail.com>
2022-12-20 17:36:50 +0000

    created login page

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-#autorisation
 
 auth = Blueprint('auth', __name__)
 
+#Authorization
+
+@auth.route('/login')
+def login():
+    from .static.loginform import loginform
+    return  render_template('login.html')
