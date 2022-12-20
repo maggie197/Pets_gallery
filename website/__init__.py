2404a0ebcb7fd4bbedef2b0d8a562aaf7de7553a
maggie197 <megijagabalina@gmail.com>
2022-12-20 16:14:29 +0000

    blueprint registred

--- website/__init__.py
+++ website/__init__.py
@@ -7,4 +7,8 @@ def create_app():        #set a function inisializing flask
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'thisisasecretkey!'# to secure cookies and sesssion data
 
+#imported blueprints
+    from .app import app
+
+    app.register_blueprint(app, url_prefix='/')
     return app
