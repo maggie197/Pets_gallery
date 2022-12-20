977dee6469ec388df3b90f13b624c34b8c159e58
maggie197 <megijagabalina@gmail.com>
2022-12-20 15:56:28 +0000

    updated flask application

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from website import create_app
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello"
-
+app = create_app(
+    
+)
 if __name__ == '__main__':
     app.run(debug=True)
