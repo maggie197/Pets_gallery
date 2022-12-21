9b2d247be6cff365075e6ffdf93a2d0259738508
maggie197 <megijagabalina@gmail.com>
2022-12-21 10:16:13 +0000

    created a sign up page

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 auth = Blueprint('auth', __name__)
@@ -8,3 +8,14 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
     return  render_template('login.html')
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+
+    return render_template("signup.html")
