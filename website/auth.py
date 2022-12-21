e6136e12b01a6a1ba0b13558d1da88bced239415
maggie197 <megijagabalina@gmail.com>
2022-12-21 10:44:33 +0000

    Modified sign up page - back end. Updated database

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
+from .modules import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash #store password that is secure
 
 
 auth = Blueprint('auth', __name__)
@@ -13,9 +16,13 @@ def login():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-
+    else:
+        new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('views.home'))
 
     return render_template("signup.html")
